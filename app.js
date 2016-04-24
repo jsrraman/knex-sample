e46384864f4ac1ce3176b57b77bd879591c57dca
Rajaraman Subramanian <rajaraman.ks@gmail.com>
2016-04-24 23:02:15 +0530

    Add raw query examples

--- app.js
+++ app.js
@@ -5,11 +5,34 @@ var screen = require('./src/util/screen')
 
 screen.clear()
 
-knex.select('title', 'rating').from('book').then(function(data) {
-	screen.write(data, 'json');
-}).catch(function(error) {
-	screen.write(error)
-}).finally(function(error) {
-// Destroy the connection pool
-	knex.destroy();
-});
+//var query = knex.select().from('book').distinct();
+
+// Raw queries example
+
+// Example 1
+//var query = knex.raw('select * from book where author_id=1')
+
+// Example 2
+// This is vulnerable to sql injection attack
+var author_id = 1
+//var query = knex.raw('select * from book where author_id = ' + author_id);
+
+// Use bindings (? will be substitued with the variable. This is safe from sql injection attack
+var query = knex.raw('select * from book where author_id = ?', [author_id]);
+
+// Example 3
+//var query = knex.select(knex.raw('COUNT(*) as BookCount')).from('book');
+
+// Run the query
+run(query, 'json');
+
+function run(knexQuery, mode) {
+	return knexQuery.then(function(data) {
+		screen.write(data.rows, mode);
+	}).catch(function(error) {
+		screen.write(error)
+	}).finally(function(error) {
+		// Destroy the connection pool
+		knex.destroy();
+	})
+};
